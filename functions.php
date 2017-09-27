f95b366fe0cfac56e0c002175909a0e4b0e2332f
Rolando Drouaillet <rdrouaillet@rol-no-roy.local>
2017-09-27 12:48:12 +0200

    fix hook

--- functions.php
+++ functions.php
@@ -316,22 +316,6 @@ function loop_actualizar_red() {
 
     $blogsie = get_last_updated(); // listar todos los hijos del multisitio
 
-    foreach ($blogsie AS $blog)
-    {   
-        switch_to_blog($blog["blog_id"]);
-
-        $blog_details = get_blog_details($blog["blog_id"]);
-
-
-        if($blog_details->blog_id >1) //entrar a cada sitio menos al principal
-        {
-            //echo $blog_details->blog_id."<br>";
-            $blogname = $blog_details->blogname;
-            $url = "http://agrega.la/".$blogname."/?fb2wp_type=all"
-            return $url."<br>";
-        }
-    }
-    restore_current_blog(); // fin del recorrido de los sitios del multisitio 
 }
  
 add_action('actualizar_red','loop_actualizar_red');
