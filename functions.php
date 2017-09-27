100c98d4e61d981e0c827e2b87c8c78b6a51ee8e
Rolando Drouaillet <rdrouaillet@rol-no-roy.local>
2017-09-27 12:51:39 +0200

    add hook blogs

--- functions.php
+++ functions.php
@@ -312,9 +312,32 @@ function actualizar_red() {
     do_action('actualizar_red');
 }
 function loop_actualizar_red() {
+    wp_reset_query();
+    wp_reset_postdata();
+    rewind_posts();
     global $wpdb; // var global para hacer queries
 
+    $contador_posts = 0;
+
     $blogsie = get_last_updated(); // listar todos los hijos del multisitio
+
+    foreach ($blogsie AS $blog)
+    {   
+        switch_to_blog($blog["blog_id"]);
+
+        $blog_details = get_blog_details($blog["blog_id"]);
+
+
+        if($blog_details->blog_id >1) //entrar a cada sitio menos al principal
+        {
+            //echo $blog_details->blog_id."<br>";
+            $blogname = $blog_details->blogname;
+            
+            echo $blogname;
+
+        }
+        restore_current_blog(); // fin del recorrido de los sitios del multisitio 
+    }
 
 }
  
