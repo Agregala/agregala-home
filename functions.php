ac2cf1624f26f307c439d643fa2547dc95eb4b4a
Rolando Drouaillet <rdrouaillet@rol-no-roy.local>
2017-09-27 13:06:40 +0200

    tsting

--- functions.php
+++ functions.php
@@ -312,34 +312,7 @@ function actualizar_red() {
     do_action('actualizar_red');
 }
 function loop_actualizar_red() {
-    wp_reset_query();
-    wp_reset_postdata();
-    rewind_posts();
-    global $wpdb; // var global para hacer queries
-
-    $contador_posts = 0;
-
-    $blogsie = get_last_updated(); // listar todos los hijos del multisitio
-
-    foreach ($blogsie AS $blog)
-    {   
-        switch_to_blog($blog["blog_id"]);
-
-        $blog_details = get_blog_details($blog["blog_id"]);
-
-
-        if($blog_details->blog_id >1) //entrar a cada sitio menos al principal
-        {
-            //echo $blog_details->blog_id."<br>";
-            $blogname = $blog_details->blogname;
-            $nombre = strtolower($blogname);
-            $url = "http://agrega.la/".$nombre."/?fb2wp_type=all"
-            $xmlData = file_get_contents($url);
-            echo $xmlData;
-
-        }
-        restore_current_blog(); // fin del recorrido de los sitios del multisitio 
-    }
+   
 }
  
 add_action('actualizar_red','loop_actualizar_red');
